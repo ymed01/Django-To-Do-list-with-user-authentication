a181445c54ef3ad0e667931a789bb5fbec215a58
ymed01 <yogeshmedthiya@gmail.com>
2024-10-19 20:55:30 +0530

    updates

--- todo_App/settings.py
+++ todo_App/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': '6543',
     }
 }
 
